43b6be9ecf0b55819f493895d2bff19c6ad6729f
danielr9911 <dani-9911@hotmail.com>
2016-05-14 11:45:59 -0500

    Graph

---
+++ src/Graph.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Graph {
+    private ArrayList<ArrayList<Integer>> vertices;
+    private ArrayList<ArrayList<Integer>> weights;
+    private int size;
+
+    public Graph() {
+        vertices = new ArrayList<ArrayList<Integer>>();
+        weights = new ArrayList<ArrayList<Integer>>();
+    }
+
+    public void addNode(String identifier) {
+        int id = Integer.parseInt(identifier);
+        ArrayList<Integer> arcs = new ArrayList<Integer>();
+        ArrayList<Integer> ws = new ArrayList<Integer>();
+        vertices.add(id, arcs);
+        weights.add(id, ws);
+    }
+
+    public int size() {
+        return vertices.size();
+    }
+
+    public void addArc(String source, String destination, String weight) {
+        int s = Integer.parseInt(source);
+        int d = Integer.parseInt(destination);
+        int w = Integer.parseInt(weight);
+        vertices.get(s).add(d);
+        weights.get(s).add(w);
+    }
+
+    public int getWeight(int source, int destination) {
+        for (int j = 0; j < size; j++) {
+            if (vertices.get(source).get(j) == destination)
+                return weights.get(source).get(j);
+        }
+        return -1;
+    }
+
+    public ArrayList<Integer> getSuccessors(int vertice) {
+        return vertices.get(vertice);
+    }
+}
--- src/RoutingMain.java
+++ src/RoutingMain.java
@@ -1,8 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by Daniel on 12/05/2016.
  */
 public class RoutingMain {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("El Viernes a las 12m");
+        Graph map = createGraph("./nodes.txt");
+        map = createArcs("./arc.txt", map);
+    }
+
+    private static Graph createGraph(String file) throws IOException {
+        Graph map = new Graph();
+        String cadena;
+        FileReader f = new FileReader(file);
+        BufferedReader b = new BufferedReader(f);
+        while ((cadena = b.readLine()) != null) {
+            map.addNode(cadena);
+        }
+        b.close();
+        return map;
+    }
+
+    private static Graph createArcs(String file, Graph map) throws IOException {
+        String cadena;
+        FileReader f = new FileReader(file);
+        BufferedReader b = new BufferedReader(f);
+        while ((cadena = b.readLine()) != null) {
+            String[] words = cadena.split(" ");
+            String source = words[0];
+            String destination = words[1];
+            String weight = words[2];
+            map.addArc(source, destination, weight);
+        }
+        b.close();
+        return map;
     }
 }

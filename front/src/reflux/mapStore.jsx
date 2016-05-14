ba07360478099d6fc5ccc33d589bde63be95bb7d
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2016-05-14 16:40:58 -0500

    map

--- front/src/reflux/mapStore.jsx
+++ front/src/reflux/mapStore.jsx
@@ -9,90 +9,64 @@ var MapStore = Reflux.createStore({
 
         this.listDir = [];
 
-        var MapboxClient = require('mapbox');
-        this.client = new MapboxClient('pk.eyJ1IjoiY3Jpc3R5YW5zdiIsImEiOiJjaW80ajZnajUwMWl5djZrcTh0MTM5Y3JzIn0.hyuT6lS1B3q2w-qdXvXXdg');
-
-        //
     },
     addPoint: function (e) {
 
-            console.log(e);
 
             var match = http.post('/match', {
-                lon: e.lngLat.lng,
-                lat: e.lngLat.lat
+                lon: e.latLng.lng(),
+                lat: e.latLng.lat()
             });
 
             match.then(function (data) {
-                if (data.status) {
+                if(data.status){
 
-                    this.client.geocodeReverse(
-                        { latitude: data.data.loc[1], longitude: data.data.loc[0] },
-                        function(err, res) {
+                    var latlng = {lat: data.data.loc[1], lng: data.data.loc[0]};
 
-                            console.log(res);
-                            var tex = res.features[0].place_name;
-                            data.data.text = tex;
+                    var marker = new google.maps.Marker({
+                        position: latlng,
+                        map: this.map,
+                        title: 'Hello World!'
+                    });
 
+                    this.geocoder.geocode({'location': latlng}, function(results, status) {
+                        if (status === google.maps.GeocoderStatus.OK) {
+
+                            data.data.text = results[0].formatted_address;
                             this.listDir.push(data.data);
 
+                            console.log(this.listDir);
+
                             this.trigger('update', this.listDir);
-                        }.bind(this));
 
-
-
-                    this.map.addSource(data.data.id, {
-                        "type": "geojson",
-                        "data": {
-                            "type": "FeatureCollection",
-                            "features": [{
-                                "type": "Feature",
-                                "geometry": {
-                                    "type": "Point",
-                                    "coordinates": [data.data.loc[0], data.data.loc[1]]
-                                },
-                                "properties": {
-                                    "marker-symbol": "marker"
-                                }
-                            }]
                         }
-                    });
+                    }.bind(this));
 
-                    this.map.addLayer({
-                        "id": data.data.id,
-                        "type": "symbol",
-                        "source": data.data.id,
-                        "layout": {
-                            "icon-image": "{marker-symbol}-15",
-                            "text-field": "{title}",
-                            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-                            "text-offset": [0, 0.6],
-                            "text-anchor": "top"
-                        }
-                    });
                 }
             }.bind(this));
     },
-    ready: function () {
+    ready: function (){
+        function loadMap() {
+            if(typeof google !== 'undefined'){
+                this.map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 6.20199, lng: -75.59398},
+                    zoom: 14
+                });
+
+                this.geocoder = new google.maps.Geocoder;
 
 
+                this.map.addListener('click', this.addPoint);
 
 
+            }else{
+                setTimeout(function () {
+                    loadMap.bind(this)();
+                }.bind(this), 20);
+            }
+        }
 
-
-        mapboxgl.accessToken = 'pk.eyJ1IjoiY3Jpc3R5YW5zdiIsImEiOiJjaW80ajZnajUwMWl5djZrcTh0MTM5Y3JzIn0.hyuT6lS1B3q2w-qdXvXXdg';
-        this.map = new mapboxgl.Map({
-            container: 'map', // container id
-            style: 'mapbox://styles/cristyansv/cio4k1wzt005nainm44yw94ug', //stylesheet location
-            center: [-75.5723, 6.20715], // starting position
-            zoom: 13,
-            hash: true
-        });
-
-
-            this.map.on('click', function (e) {
-            this.addPoint(e);
-        }.bind(this));
+        loadMap.bind(this)();
     }
 });
 

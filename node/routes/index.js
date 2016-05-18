e7ae59114014939728a325e49d471a325a7e8dc8
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2016-05-18 13:56:03 -0500

    fixe

--- node/routes/index.js
+++ node/routes/index.js
@@ -62,6 +62,8 @@ router.post('/route', function (req, res) {
 
         prc.stdout.setEncoding('utf8');
         prc.stdout.on('data', function (data) {
+
+            console.log('data');
             solution += data;
         });
 
